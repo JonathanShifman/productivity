f598d5b4b772678c14dd74de9b5a05bb109a4ba1
Jonathan <jonathan.shifman@gmail.com>
2018-08-18 15:24:18 +0300

    Initialized config file and loading financial data from JSON

--- front-end/src/app/Components/app/app.component.ts
+++ front-end/src/app/Components/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  config;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const configFileObservable = this.http.get('assets/config/config.json');
+    configFileObservable.subscribe(configTree => {
+      this.config = configTree;
+      if (this.config['shouldLoadFromJSON']) {
+        this.loadJSONs();
+      }
+    });
+  }
+
+  loadJSONs() {
+    const fileObservable = this.http.get('assets/data/financial.json');
+    fileObservable.subscribe(tree => {
+      localStorage.setItem('financial', JSON.stringify(tree));
+    });
+  }
+
 }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app/app.component';
 import { FinancialComponent } from './Components/financial/financial.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { FinancialComponent } from './Components/financial/financial.component';
     FinancialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
